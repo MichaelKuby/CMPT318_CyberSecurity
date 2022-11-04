18fb48205d8051e9163498b940be2e24c4749ec3
MichaelKuby <105996614+MichaelKuby@users.noreply.github.com>
2022-11-04 11:10:46 -0700

    Update Assignment 2

--- CMPT318_Assignment_2/Group Assignment 2/CMPT318_Assignment_2.R
+++ CMPT318_Assignment_2/Group Assignment 2/CMPT318_Assignment_2.R
@@ -21,6 +21,7 @@ df <- data[data$Date >= as.POSIXlt("2007-01-01") & data$Date <= as.POSIXlt("2007
 dfsplit <- strsplit(df[,2], ",") # split the data
 df2 <- data.frame(dfsplit) # Put the data into a df
 df2 <- as.data.frame(t(df2)) #transpose to turn rows into cols
+rm(dfsplit)
 
 # Reindexing with help from James Comment: 
 # https://stackoverflow.com/questions/7567790/change-the-index-number-of-a-dataframe
@@ -52,20 +53,51 @@ n = 52
 nw = 1
 msn = 10080
 ms = 10
-dfw <- data.frame(Num_week=df$Global_intensity, Time=df$Time, Moving_average=df$Global_intensity)
+df_ma <- data.frame(Num_week=df$Global_intensity, Moving_average=df$Global_intensity)
 
 # breakdown the average calculations per week 
 for (i in 1:n) {
   nStart = i*10080 - 10079
   nEnd = nStart + 10079
-  dfw[nStart:nEnd, 1] = i
-  dfw[nStart:nEnd, 2] = ma(df$Global_intensity[nStart:nEnd], ms, centre = TRUE)
+  df_ma[nStart:nEnd, 1] = i
+  df_ma[nStart:nEnd, 2] = ma(df$Global_intensity[nStart:nEnd], ms, centre = TRUE)
 }
-dfw$Day <- df$Day
-dfw <- na.omit(dfw)           # remove all incomplete cases of a data object
+df_ma$Day <- df$Day
+df_ma$Time <- df$Time
 
 # create average week
 df3 <- na.omit(df)           # remove all incomplete cases of a data object
 average_week <- aggregate(x = df3$Global_intensity, by = list(df3$Time, df3$Day), FUN = mean, simplify = TRUE, drop = TRUE)
 colnames(average_week) <- c("Time", "Day", "Averaged Global_intensity")
 
+# manually reorder average_week
+order <- c("Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday", "Sunday")
+average_week = average_week %>%
+  mutate(Day =  factor(Day, levels = order)) %>%
+  arrange(Day)
+
+for (i in 1:n)
+{
+  df_ma[df_ma$Num_week == i, "Squared_error"] <- (average_week$`Averaged Global_intensity` - df_ma[df_ma$Num_week == i, "Moving_average"])^2
+}
+
+df_ma2 <- na.omit(df_ma)
+
+# aggregate by the week, taking the sum of squared errors
+weekly_error <- aggregate(x = df_ma2$Squared_error, by = list(df_ma2$Num_week), FUN = sum, simplify = TRUE)
+colnames(weekly_error) <- c("Week", "Sum of Squared Errors")
+
+
+for (i in 1:n)
+{
+  weekly_error[weekly_error$Week == i, "Mean Squared Error"] <- weekly_error[weekly_error$Week == i, "Sum of Squared Errors"] / length(which(df_ma2$Num_week == i))
+}
+
+weekly_error$rank <- rank(weekly_error$`Mean Squared Error`)
+
+# Most anomalous week is week 52 given by the greatest MSE
+# Least anomalous week is week 17 given by the lowest MSE
+
+# Plot the smoothened versions of the most and least anomalous weeks against the average smoothened week
+
+df_plot <- data.frame(Time=average_week$Time, Week_17 = df_ma[df_ma$Num_week == 17, "Moving_average"], Week_52 = df_ma[df_ma$Num_week == 52, "Moving_average"])
