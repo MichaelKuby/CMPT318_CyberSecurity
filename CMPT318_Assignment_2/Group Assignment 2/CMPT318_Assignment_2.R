803d355050d688ddcfc5dcec1bef6869b9597961
MichaelKuby <105996614+MichaelKuby@users.noreply.github.com>
2022-11-04 14:01:33 -0700

    update

--- CMPT318_Assignment_2/Group Assignment 2/CMPT318_Assignment_2.R
+++ CMPT318_Assignment_2/Group Assignment 2/CMPT318_Assignment_2.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(ggcorrplot)
 library(lubridate)
 library(forecast)
+library(zoo)
 
 # Set the proper working directory
 setwd("/Users/MichaelKuby/Documents/GitHub/CMPT318_CyberSecurity/CMPT318_Assignment_2/Group Assignment 2")
@@ -47,28 +48,30 @@ df$Day = weekdays(as.Date(df$Date))
 
 #Convert char to double
 df$Global_intensity <- as.double(df$Global_intensity) # Convert char to double
+rm(df2)
+rm(data)
 
 # n weeks, ms samples and msn number of samples returned for the week
 n = 52
 nw = 1
 msn = 10080
 ms = 10
-df_ma <- data.frame(Num_week=df$Global_intensity, Moving_average=df$Global_intensity)
+df_ma <- data.frame(Date = df$Date, 
+                    Num_week=strftime(df$Date, format = "%V"), 
+                    Day = strftime(df$Date, format = "%A"),
+                    Time = strftime(df$Date, format = "%T"),
+                    Moving_average=df$Global_intensity)
+
+df_ma$Num_week <- sub("^0", "", df_ma$Num_week)
 
 # breakdown the average calculations per week 
-for (i in 1:n) {
-  nStart = i*10080 - 10079
-  nEnd = nStart + 10079
-  df_ma[nStart:nEnd, 1] = i
-  df_ma[nStart:nEnd, 2] = ma(df$Global_intensity[nStart:nEnd], ms, centre = TRUE)
-}
-df_ma$Day <- df$Day
-df_ma$Time <- df$Time
+df_ma$Moving_average <- ma(df$Global_intensity, ms, centre = TRUE)
 
 # create average week
-df3 <- na.omit(df)           # remove all incomplete cases of a data object
-average_week <- aggregate(x = df3$Global_intensity, by = list(df3$Time, df3$Day), FUN = mean, simplify = TRUE, drop = TRUE)
-colnames(average_week) <- c("Time", "Day", "Averaged Global_intensity")
+average_week <- df_ma %>% group_by(Day, Time)
+average_week <- average_week %>% summarise(
+  Moving_average = mean(Moving_average, na.rm = TRUE)
+)
 
 # manually reorder average_week
 order <- c("Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday", "Sunday")
@@ -76,21 +79,23 @@ average_week = average_week %>%
   mutate(Day =  factor(Day, levels = order)) %>%
   arrange(Day)
 
-for (i in 1:n)
-{
-  df_ma[df_ma$Num_week == i, "Squared_error"] <- (average_week$`Averaged Global_intensity` - df_ma[df_ma$Num_week == i, "Moving_average"])^2
-}
+# Compute Squared Error
 
+for (i in 01:n)
+{
+  df_ma[df_ma$Num_week == i, "Squared_error"] <- (average_week$'Moving_average' - df_ma[df_ma$Num_week == i, "Moving_average"])^2
+}
 df_ma2 <- na.omit(df_ma)
 
 # aggregate by the week, taking the sum of squared errors
-weekly_error <- aggregate(x = df_ma2$Squared_error, by = list(df_ma2$Num_week), FUN = sum, simplify = TRUE)
-colnames(weekly_error) <- c("Week", "Sum of Squared Errors")
+weekly_error <- df_ma2 %>% group_by(Num_week)
+weekly_error <- weekly_error %>% summarise(
+  Sum_of_Squared_Errors = sum(Squared_error)
+)
 
-
-for (i in 1:n)
+for (j in 1:n)
 {
-  weekly_error[weekly_error$Week == i, "Mean Squared Error"] <- weekly_error[weekly_error$Week == i, "Sum of Squared Errors"] / length(which(df_ma2$Num_week == i))
+  weekly_error[weekly_error$Week == j, "Mean Squared Error"] <- weekly_error[weekly_error$Week == j, "Sum of Squared Errors"] / length(which(df_ma2$Num_week == j))
 }
 
 weekly_error$rank <- rank(weekly_error$`Mean Squared Error`)
@@ -100,4 +105,28 @@ weekly_error$rank <- rank(weekly_error$`Mean Squared Error`)
 
 # Plot the smoothened versions of the most and least anomalous weeks against the average smoothened week
 
-df_plot <- data.frame(Time=average_week$Time, Week_17 = df_ma[df_ma$Num_week == 17, "Moving_average"], Week_52 = df_ma[df_ma$Num_week == 52, "Moving_average"])
+dates <- data.frame(Date=df$Date[df$Date >= as.POSIXlt("2007-04-23") & df$Date < as.POSIXlt("2007-04-30")])
+dates <- na.omit(dates)
+
+df_plot <- data.frame(Date = dates,
+                      Week_17 = df_ma[df_ma$Num_week == 17, "Moving_average"], 
+                      Week_52 = df_ma[df_ma$Num_week == 52, "Moving_average"], 
+                      Average_Week = average_week$`Moving_average`)
+
+# I noticed that week 17 has a lot of NA data points. So the following is to check the original data to see if 
+# this was an error produced somewhere, or present in the original data. Week 17 is april 23 to may 29 
+# sum(is.na(df$Global_intensity[df$Date >= as.POSIXlt("2007-04-23") & df$Date < as.POSIXlt("2007-04-30")]))
+# sum(is.na(df_plot$Week_17))
+# I can see in the original data that there are 2919 NA's for week 17 global intensity. Strangely in the smoothened data we are missing less at 2869.
+# I assume the moving average technique allows for certain points that would have been NA to actually obtain a value, which is kind of interesting.
+
+ggplot(data = df_plot) +
+  geom_point(mapping = aes(x = Date, y = Week_17, color = "Least Anomalous (Week 17)")) +
+  geom_point(mapping = aes(x = Date, y = Week_52, color = "Most Anomalous (Week 52)")) +
+  geom_point(mapping = aes(x = Date, y = Average_Week, color = "Average Week"))+
+  labs( title = "Smoothened Global Intensity vs. Time") +
+  guides(color = guide_legend(title = "Colour Guide")) +
+  xlab("Time") +
+  ylab("Global Intensity (Amperes)")
+
+weekly_error <- subset (weekly_error, select = c( "Week", "Mean Squared Error", "rank" ))
